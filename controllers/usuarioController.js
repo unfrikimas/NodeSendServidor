6344bdcfb8043c6cc9e8d8176626d90f9194ba46
Carlos Enrique Herrera <carlos@unfrikimas.com>
2020-11-29 16:57:05 +0100

    Evitar usuarios duplicados

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -2,9 +2,26 @@ const Usuario = require('../models/Usuario');
 
 exports.nuevoUsuario = async (req, res) => {
 
-    const usuario = await new Usuario(req.body)
-    usuario.save();
+    //verificar si el email del usuario ya esta registrado
+    const { email, password } = req.body;
 
-    res.json({msg: 'Usuario creado correctamente'});
+    try {
+        
+        let usuario = await Usuario.findOne({ email });
+
+        if(usuario) {
+            return res.status(400).json({ msg: 'El usuario ya está registrado!' });
+        } 
+        
+        //guarda el usuario
+        usuario = new Usuario(req.body)
+        await usuario.save();
+        res.json({msg: 'Usuario creado correctamente'});   
+
+    } catch (error) {
+        console.log(error);
+        res.status(400).send('Hubo un error');
+    }
+
 
 }
