776e48a7d361f42ef2380f4a13a2841e51736506
Carlos Enrique Herrera <carlos@unfrikimas.com>
2020-11-29 18:07:22 +0100

    Verificando si el usuario existe

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,22 @@
-
+const Usuario = require('../models/Usuario');
 
 exports.autenticarUsuario = async (req, res, next) => {
+
+    //Revisar si hay errores
+
+    //Buscar el usuario para verificar si esta registrado
+    const { email } = req.body;
+    const usuario = await Usuario.findOne({ email });
+
+    if(!usuario) {
+        res.status(401).json({ msg: 'El usuario no existe' });
+        return next();
+    }
+
+    //Verificar el password y autenticar el usuario
 
 }
 
 exports.usuarioAutenticado = (req, res) => {
-    
+
 }
