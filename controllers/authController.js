375765dcb0b3baca2ba4fb217820829ecb295ca2
Carlos Enrique Herrera <carlos@unfrikimas.com>
2020-11-29 20:06:20 +0100

    Agregando validacion a la autenticacion

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,10 +2,15 @@ const Usuario = require('../models/Usuario');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 require('dotenv').config({ path: 'variables.env' });
+const { validationResult } = require('express-validator');
 
 exports.autenticarUsuario = async (req, res, next) => {
 
-    //Revisar si hay errores
+    // revisar si hay errores
+    const errores = validationResult(req);
+    if(!errores.isEmpty()) {
+        return res.status(400).json({ errores: errores.array() })
+    }       
 
     //Buscar el usuario para verificar si esta registrado
     const { email, password } = req.body;
--- routes/auth.js
+++ routes/auth.js
@@ -4,9 +4,12 @@ const authController = require('../controllers/authController');
 const { check } = require('express-validator');
 
 router.post('/',
+    [
+        check('email', 'Agrega un email válido').isEmail(),
+        check('password', 'Password no puede ir vacío').not().isEmpty()
+    ],
     authController.autenticarUsuario
 );
-
 
 router.get('/',
     authController.usuarioAutenticado
