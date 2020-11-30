7c2861adeed644030db61d4a11cd183709b59d3f
Carlos Enrique Herrera <carlos@unfrikimas.com>
2020-11-30 16:59:01 +0100

    Definiendo numero descargas y hash al password

--- controllers/enlacesController.js
+++ controllers/enlacesController.js
@@ -1,5 +1,6 @@
 const Enlace = require('../models/Enlace');
 const shortid = require('shortid');
+const bcrypt = require('bcrypt');
 
 exports.nuevoEnlace = async (req, res, next) => {    
 
@@ -12,7 +13,25 @@ exports.nuevoEnlace = async (req, res, next) => {
     enlace.url = shortid.generate();
     enlace.nombre = shortid.generate();
     enlace.nombre_original = nombre_original;
-    enlace.password = password;
+
+    //revisa si el usuario esta autenticado
+    if(req.usuario) {
+        const { password, descargas } = req.body
+
+        //asignar a enlace el numero de descargas
+        if(descargas) {
+            enlace.descargas = descargas;
+        }
+
+        //asignar un password
+        if(password) {
+            const salt = await bcrypt.genSalt(10);
+            enlace.password = await bcrypt.hash(password, salt);
+        }
+
+        //asignar el autor
+        enlace.autor = req.usuario.id
+    }
 
     //almacenar el enlace en la DB
     try {
