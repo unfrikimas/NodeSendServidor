02a72a051db569e838c87f32d687cca680f99f85
Carlos Enrique Herrera <carlos@unfrikimas.com>
2020-11-30 18:50:30 +0100

    Configuracion de parametros en multer

--- controllers/archivosController.js
+++ controllers/archivosController.js
@@ -1,6 +1,38 @@
+const multer = require('multer');
+const shortid = require('shortid');
 
+const configuracionMulter = {
+    limits: { fileSize: 1000000 },
+    storage: fileStorage = multer.diskStorage({
+        destination: (req, file, cb) => {
+            cb(null, __dirname+'/../uploads')
+        },
+        filename: (req, file, cb) => {
+            const extension = file.mimetype.split('/')[1];
+            cb(null, `${shortid.generate()}.${extension}`);
+        },
+        //filtrar por formato de archivos
+        // fileFilter: (req, file, cb) => {
+        //     if(file.mimetype === 'application/pdf') {
+        //         return cb(null, true);
+        //     }
+        // }
+    })
+}
 
-exports.subirArchivo = async (req, res) => {
+const upload = multer(configuracionMulter).single('archivo');
+
+exports.subirArchivo = async (req, res, next) => {
+
+    upload(req, res, async(error) => {
+        console.log(req.file)
+        if(!error) {
+            res.json({ archivo: req.file.filename });
+        } else {
+            console.log(error);
+            return next();
+        }
+    })
 
 }
 
--- routes/archivos.js
+++ routes/archivos.js
@@ -3,10 +3,6 @@ const router = express.Router();
 const archivosController = require('../controllers/archivosController');
 const auth = require('../middleware/auth');
 
-//subida de archivos
-const multer = require('multer');
-const upload = multer({ dest: './uploads' });
-
 router.post('/',
     archivosController.subirArchivo
 );
