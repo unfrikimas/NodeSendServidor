0905d7656a28205bfdd529db6ac21f08b7aa0c59
Carlos Enrique Herrera <carlos@unfrikimas.com>
2020-11-30 19:09:05 +0100

    Agregando la extension de los archivos

--- controllers/archivosController.js
+++ controllers/archivosController.js
@@ -1,31 +1,32 @@
 const multer = require('multer');
 const shortid = require('shortid');
 
-const configuracionMulter = {
-    limits: { fileSize: 1000000 },
-    storage: fileStorage = multer.diskStorage({
-        destination: (req, file, cb) => {
-            cb(null, __dirname+'/../uploads')
-        },
-        filename: (req, file, cb) => {
-            const extension = file.mimetype.split('/')[1];
-            cb(null, `${shortid.generate()}.${extension}`);
-        },
-        //filtrar por formato de archivos
-        // fileFilter: (req, file, cb) => {
-        //     if(file.mimetype === 'application/pdf') {
-        //         return cb(null, true);
-        //     }
-        // }
-    })
-}
-
-const upload = multer(configuracionMulter).single('archivo');
-
 exports.subirArchivo = async (req, res, next) => {
 
+    const configuracionMulter = {
+        limits: { fileSize: req.usuario ? 1024 * 1024 * 10 : 1024 * 1024 },
+        storage: fileStorage = multer.diskStorage({
+            destination: (req, file, cb) => {
+                cb(null, __dirname+'/../uploads')
+            },
+            filename: (req, file, cb) => {
+                // const extension = file.mimetype.split('/')[1];
+                const extension = file.originalname.substring(file.originalname.lastIndexOf('.'), file.originalname.length);
+                cb(null, `${shortid.generate()}${extension}`);
+            },
+            //filtrar por formato de archivos
+            // fileFilter: (req, file, cb) => {
+            //     if(file.mimetype === 'application/pdf') {
+            //         return cb(null, true);
+            //     }
+            // }
+        })
+    }    
+
+    const upload = multer(configuracionMulter).single('archivo');
+
     upload(req, res, async(error) => {
-        console.log(req.file)
+        // console.log(req.file)
         if(!error) {
             res.json({ archivo: req.file.filename });
         } else {
