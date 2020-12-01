3cca49d210cdaecd05e236479d28484e4935836e
Carlos Enrique Herrera <carlos@unfrikimas.com>
2020-12-01 21:29:03 +0100

    Agregando variable de entorno para proteger la api

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ const app = express();
 conectarDB();
 
 //habilitar cors
-app.use( cors() );
+const opcionesCors = {
+    origin: process.env.FRONTEND_URL
+}
+app.use( cors(opcionesCors) );
 
 //puerto de la app
 const port = process.env.PORT || 4000;
